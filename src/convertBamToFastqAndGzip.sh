ac852d7c6653bfd3f6e624a24d29af39b9fa7b82
yh <yh@alder>
2011-08-03 17:51:12 -0700

    modify to take one bam filename as 1st argument and output filename prefix as 2nd argument

--- src/convertBamToFastqAndGzip.sh
+++ src/convertBamToFastqAndGzip.sh
@@ -1,24 +1,21 @@
 #!/bin/sh
-#$ -pe mpich 5
 
-if test $# -lt 1
+if test $# -lt 2
 then
-	echo "Usage: $0 INPUTDIR"
+	echo "Usage: $0 INPUTFNAME OUTPUTFnamePREFIX"
 	echo
-	echo "	Given sam or bam (*.?am) files in INPUTDIR which contain paired-end reads,"
-	echo "	  1. call picard to convert them to fastq."
-	echo "	  2. gzip all fastq files"
+	echo "	Given a sam or bam (*.?am) file which contains paired-end reads,"
+	echo "	  1. call picard to convert them to two fastq files."
+	echo "	  2. gzip the fastq files"
 	echo "Note:
-	1. '.' could only be included once in the INPUTDIR, i.e. in the beginning.
-	   because it is used as a separator in awk to extract filename prefix.
 		"
 	echo
-	echo "Examples:	~/script//shell/convertBamToFastqAndGzip.sh ./fastq/"
+	echo "Examples:	~/script//shell/convertBamToFastqAndGzip.sh ./fastq/gerald_81GPJABXX_5_TTAGGC.bam db/individual_sequence/1_3"
 exit
 fi
 
-inputDir=$1
-#outputDir=$2
+inputFname=$1
+outputFnamePrefix=$2
 
 parameter=''
 #while test -n "$6"
@@ -26,19 +23,25 @@ parameter=''
 #parameter=$parameter' '$6
 #shift
 #done
+picard_tool_path=~/script/picard/dist/
+java -jar $picard_tool_path/SamToFastq.jar INPUT=$inputFname F=$outputFnamePrefix\_1.fastq F2=$outputFnamePrefix\_2.fastq
 
-for i in `ls $inputDir/*.?am`; do echo $i;
-	fname_prefix=`echo $i|awk -F . '{print $1}'`
-	if test -z $fname_prefix	# . is the first letter in the directory name
-	then
-		fname_prefix=.`echo $i|awk -F . '{print $2}'`
-	fi
-	input_fname=$i
-	output_prefix=$fname_prefix
-	java -jar ~/script/vervet-web/bin/picard-tools/SamToFastq.jar INPUT=$input_fname F=$output_prefix\_1.fastq F2=$output_prefix\_2.fastq
-done
+gzip $outputFnamePrefix\_1.fastq
+gzip $outputFnamePrefix\_2.fastq
 
-for i in `ls $inputDir/*.fastq`; do echo $i;
-	input_fname=$i
-	gzip $input_fname
-done
+
+#for i in `ls $inputDir/*.?am`; do echo $i;
+#	fname_prefix=`echo $i|awk -F . '{print $1}'`
+#	if test -z $fname_prefix	# . is the first letter in the directory name
+#	then
+#		fname_prefix=.`echo $i|awk -F . '{print $2}'`
+#	fi
+#	input_fname=$i
+#	output_prefix=$fname_prefix
+#	java -jar ~/script/vervet/bin/picard-tools/SamToFastq.jar INPUT=$input_fname F=$output_prefix\_1.fastq F2=$output_prefix\_2.fastq
+#done
+#
+#for i in `ls $inputDir/*.fastq`; do echo $i;
+#	input_fname=$i
+#	gzip $input_fname
+#done
