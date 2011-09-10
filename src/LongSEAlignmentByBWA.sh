98f34d46910bd3be13fc606b93f25cad8a647797
yh <yh@alder>
2011-09-09 22:39:21 -0700

    add one optional argument in the end: noOfAlnThreads

--- src/LongSEAlignmentByBWA.sh
+++ src/LongSEAlignmentByBWA.sh
@@ -3,11 +3,11 @@
 noOfAlnThreads=4
 if test $# -lt 3
 then
-	echo "Usage: $0 refFastaFname fastqF1 outputBamFnamePrefix"
+	echo "Usage: $0 refFastaFname fastqF1 outputBamFnamePrefix [noOfAlnThreads]"
 	echo
 	echo "Note:"
 	echo "	1. refFastaFname must have been indexed by bwa."
-	echo "	2. This script uses $noOfAlnThreads threads in aln."
+	echo "	2. This script uses $noOfAlnThreads threads in bwaw by default."
 	echo "	3. Unmapped reads will be removed."
 	echo "	4. Final bam output is sorted. sorting uses 2G mem."
 	echo "Example:"
@@ -20,5 +20,10 @@ samtoolsPath=~/bin/samtools
 refFastaFname=$1
 fastqF1=$2
 outputBamFnamePrefix=$3
+
+if test -n "$4"
+then
+	noOfAlnThreads=$4
+fi
 
 $bwaPath bwasw  -t $noOfAlnThreads $refFastaFname $fastqF1 | $samtoolsPath view  -F 4 -bSh - | $samtoolsPath sort -m 2000000000 - $outputBamFnamePrefix
