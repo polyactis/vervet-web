0455f3229a84cda331425113f2928cfab44838f7
yh <yh@alder>
2011-09-09 22:47:27 -0700

    "bwa aln" is split out. now this is samse + sam 2 bam conversion + sort. saiF1 is added as one argument

--- src/ShortSEAlignmentByBWA.sh
+++ src/ShortSEAlignmentByBWA.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # 2011-9-7 bwa alignment through unix pipe for short-read single-end data
 noOfAlnThreads=4
-if test $# -lt 3
+if test $# -lt 4
 then
-	echo "Usage: $0 refFastaFname fastqF1 outputBamFnamePrefix"
+	echo "Usage: $0 refFastaFname saiF1 fastqF1 outputBamFnamePrefix"
 	echo
 	echo "Note:"
 	echo "	1. refFastaFname must have been indexed by bwa."
-	echo "	2. This script uses $noOfAlnThreads threads in aln."
-	echo "	3. Unmapped reads will be removed."
-	echo "	4. Final bam output is sorted. sorting uses 2G mem."
+	echo "	2. Unmapped reads will be removed."
+	echo "	3. Final bam output is sorted. sorting uses 2G mem."
 	echo "Example:"
 	echo "	$0 120_480K_supercontigs.fasta individual_sequence/435_6136_2004030_GA_5/gerald_81GPBABXX_8_CGATGT_1.fastq.gz 435_6136_2004030_GA_5/gerald_81GPBABXX_8_CGATGT"
 exit
@@ -18,7 +17,8 @@ fi
 bwaPath=~/bin/bwa
 samtoolsPath=~/bin/samtools
 refFastaFname=$1
-fastqF1=$2
-outputBamFnamePrefix=$3
+saiF1=$2
+fastqF1=$3
+outputBamFnamePrefix=$4
 
-$bwaPath samse $refFastaFname <($bwaPath aln -t $noOfAlnThreads $refFastaFname $fastqF1) $fastqF1 | $samtoolsPath view  -F 4 -bSh - | $samtoolsPath sort -m 2000000000 - $outputBamFnamePrefix
+$bwaPath samse $refFastaFname $saiF1 $fastqF1 | $samtoolsPath view  -F 4 -bSh - | $samtoolsPath sort -m 2000000000 - $outputBamFnamePrefix
