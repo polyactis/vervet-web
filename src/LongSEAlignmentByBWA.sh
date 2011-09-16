5e074e19dfb3c48c03e342fe8956d3e4dd3e9c9a
yh <yh@alder>
2011-09-15 21:26:00 -0700

    handle the exit code from different stages of pipe more securely and gracefully

--- src/LongSEAlignmentByBWA.sh
+++ src/LongSEAlignmentByBWA.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-# 2011-9-7 bwa alignment through unix pipe for long-read single-end data
+# 2011-9-7 bwasw alignment piped to gzipped sam for long-read single-end data
 noOfAlnThreads=4
 if test $# -lt 3
 then
-	echo "Usage: $0 refFastaFname fastqF1 outputBamFnamePrefix [noOfAlnThreads]"
+	echo "Usage: $0 refFastaFname fastqF1 outputSamGzipFname [noOfAlnThreads]"
 	echo
 	echo "Note:"
 	echo "	1. refFastaFname must have been indexed by bwa."
-	echo "	2. This script uses $noOfAlnThreads threads in bwaw by default."
-	echo "	3. Unmapped reads will be removed."
-	echo "	4. Final bam output is sorted. sorting uses 2G mem."
+	echo "	2. This script uses $noOfAlnThreads threads in bwasw by default."
 	echo "Example:"
-	echo "	$0 120_480K_supercontigs.fasta individual_sequence/1_VRC_ref_454/GINC65G04.fastq.gz 1_VRC_ref_454/GINC65G04"
+	echo "	$0 120_480K_supercontigs.fasta individual_sequence/1_VRC_ref_454/GINC65G04.fastq.gz 1_VRC_ref_454/GINC65G04.sam.gz"
 exit
 fi
 
@@ -19,11 +17,24 @@ bwaPath=~/bin/bwa
 samtoolsPath=~/bin/samtools
 refFastaFname=$1
 fastqF1=$2
-outputBamFnamePrefix=$3
+outputSamGzipFname=$3
 
 if test -n "$4"
 then
 	noOfAlnThreads=$4
 fi
 
-$bwaPath bwasw  -t $noOfAlnThreads $refFastaFname $fastqF1 | $samtoolsPath view  -F 4 -bSh - | $samtoolsPath sort -m 2000000000 - $outputBamFnamePrefix
+#$bwaPath bwasw  -t $noOfAlnThreads $refFastaFname $fastqF1 | $samtoolsPath view  -F 4 -bSh - | $samtoolsPath sort -m 2000000000 - $outputBamFnamePrefix
+$bwaPath bwasw  -t $noOfAlnThreads $refFastaFname $fastqF1 | gzip >$outputSamGzipFname
+
+exitCodeAll="${PIPESTATUS[0]} ${PIPESTATUS[1]}"
+exitCode=`echo $exitCodeAll|awk -F ' ' '{print $1}'`
+exitCode2=`echo $exitCodeAll|awk -F ' ' '{print $2}'`
+echo exit code: $exitCode, $exitCode2
+if test "$exitCode" = "0" && test "$exitCode2" = "0"
+then
+	#normal exit
+	exit
+else
+	exit $exitCodeAll
+fi
--- src/ShortSEAlignmentByBWA.sh
+++ src/ShortSEAlignmentByBWA.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# 2011-9-7 bwa alignment through unix pipe for short-read single-end data
+# 2011-9-7 bwa sampe piped to gzipped sam for single-end short-read
 noOfAlnThreads=4
 if test $# -lt 4
 then
-	echo "Usage: $0 refFastaFname saiF1 fastqF1 outputBamFnamePrefix"
+	echo "Usage: $0 refFastaFname saiF1 fastqF1 outputSamGzipFname"
 	echo
 	echo "Note:"
 	echo "	1. refFastaFname must have been indexed by bwa."
-	echo "	2. Unmapped reads will be removed."
-	echo "	3. Final bam output is sorted. sorting uses 2G mem."
 	echo "Example:"
-	echo "	$0 120_480K_supercontigs.fasta individual_sequence/435_6136_2004030_GA_5/gerald_81GPBABXX_8_CGATGT_1.fastq.gz 435_6136_2004030_GA_5/gerald_81GPBABXX_8_CGATGT"
+	echo "	$0 120_480K_supercontigs.fasta individual_sequence/435_6136_2004030_GA_5/gerald_81GPBABXX_8_CGATGT_1.fastq.gz 435_6136_2004030_GA_5/gerald_81GPBABXX_8_CGATGT.sam.gz"
 exit
 fi
 
@@ -19,6 +17,18 @@ samtoolsPath=~/bin/samtools
 refFastaFname=$1
 saiF1=$2
 fastqF1=$3
-outputBamFnamePrefix=$4
+outputSamGzipFname=$4
 
-$bwaPath samse $refFastaFname $saiF1 $fastqF1 | $samtoolsPath view  -F 4 -bSh - | $samtoolsPath sort -m 2000000000 - $outputBamFnamePrefix
+#$bwaPath samse $refFastaFname $saiF1 $fastqF1 | $samtoolsPath view  -F 4 -bSh - | $samtoolsPath sort -m 2000000000 - $outputBamFnamePrefix
+$bwaPath samse $refFastaFname $saiF1 $fastqF1 | gzip >$outputSamGzipFname
+exitCodeAll="${PIPESTATUS[0]} ${PIPESTATUS[1]}"
+exitCode=`echo $exitCodeAll|awk -F ' ' '{print $1}'`
+exitCode2=`echo $exitCodeAll|awk -F ' ' '{print $2}'`
+echo exit code: $exitCode, $exitCode2
+if test "$exitCode" = "0" && test "$exitCode2" = "0"
+then
+	#normal exit
+	exit
+else
+	exit $exitCodeAll
+fi
