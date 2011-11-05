815ef21804968568de77f982a9648a12f2651dba
crocea <crocea@uclaOffice>
2011-11-04 17:23:48 -0700

    for samtools mpileup 1. add '-Q 20' and increase 20 to 30 in '-q 30' 2. remove "-I" so that indels would be called too
    add 4th commandline argument siteType
    +	echo "	4. siteType. 1: all sites; 2: variants only"
    
    but "all sites" (remove -v in bcftools) for samtools is not really working.
    
    add "-w 10 -d 3" in "~/bin/vcfutils.pl varFilter "

--- src/CallVariantBySamtools.sh
+++ src/CallVariantBySamtools.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # 2011-9-14 call variants through samtools
-if test $# -lt 4
+if test $# -lt 5
 then
-	echo "Usage: $0 refFastaFname interval outputVCF inputBAM1 inputBAM2 ..."
+	echo "Usage: $0 refFastaFname interval outputVCF siteType inputBAM1 inputBAM2 ..."
 	echo
 	echo "Note:"
 	echo "	1. bamListFile contains a list of bam filenames, one file per line."
 	echo "	2. refFastaFname must be faidx-indexed."
 	echo "	3. shell has to be bash. PIPESTATUS is used."
+	echo "	4. siteType. 1: all sites; 2: variants only"
 	echo
 	echo "Example:"
-	echo "	$0 120_480K_supercontigs.fasta Contig0:1-2000000 Contig0_1_2000000.vcf 1_vs_120.bam 2_vs_120.bam"
+	echo "	$0 120_480K_supercontigs.fasta Contig0:1-2000000 Contig0_1_2000000.vcf 1 1_vs_120.bam 2_vs_120.bam"
 exit
 fi
 
@@ -24,6 +25,8 @@ picardPath=~/script/picard/dist/
 refFastaFname=$1
 interval=$2
 outputVCF=$3
+siteType=$4
+shift
 shift
 shift
 shift
@@ -34,6 +37,7 @@ bamFiles=$*
 #-D:	instructs mpileup to output per-sample read depth.
 #-S:	Output per-sample Phred-scaled strand bias P-value
 #-q 20:	Minimum mapping quality for an alignment to be used
+#-Q 20: Minimum base quality for a base to be considered
 #-C 50:	Coefficient for downgrading mapping quality for reads containing excessive mismatches. Given a read with a phred-scaled probability q of being generated from the mapped position, the new mapping quality is about sqrt((INT-q)/INT)*INT. A zero value disables this functionality; if enabled, the recommended value for BWA is 50. [0]
 #-d 250:	At a position, read maximally INT reads per input BAM.
 #-u in -ug:	Similar to -g except that the output is uncompressed BCF, which is preferred for piping.
@@ -45,7 +49,14 @@ bamFiles=$*
 #-g in "-vcg":	Call per-sample genotypes at variant sites (force -c)
 #-D100: maximum depth=100
 
-$samtoolsPath mpileup -I -S -D -q 20 -ug -r $interval -f $refFastaFname $bamFiles | $bcftoolsPath view -bvcg - > $outputVCF.bcf
+if test "$siteType" = "1"
+then
+	bcftoolsArguments="-bcg"
+else
+	bcftoolsArguments="-bvcg"
+fi
+
+$samtoolsPath mpileup -S -D -q 30 -Q 20 -ug -r $interval -f $refFastaFname $bamFiles | $bcftoolsPath view $bcftoolsArguments - > $outputVCF.bcf
 
 exitCodeAll="${PIPESTATUS[0]} ${PIPESTATUS[1]}"
 exitCode=`echo $exitCodeAll|awk -F ' ' '{print $1}'`
@@ -53,7 +64,7 @@ exitCode2=`echo $exitCodeAll|awk -F ' ' '{print $2}'`
 echo exit code: $exitCode, $exitCode2
 if test "$exitCode" = "0" && test "$exitCode2" = "0"
 then
-	$bcftoolsPath view $outputVCF.bcf | $vcfutilsPath varFilter -D5000 > $outputVCF
+	$bcftoolsPath view $outputVCF.bcf | $vcfutilsPath varFilter -w 10 -d 3 -D5000 > $outputVCF
 	exitCode=$?
 	rm $outputVCF.bcf
 	exit $exitCode
