3ebcdd50463d5da3c2799c6464dae9008f203fc5
crocea <crocea@uclaOffice>
2011-11-17 18:40:03 -0800

    refine these plotting programs

--- src/plot/PlotVCFAAF_vs_Position.py
+++ src/plot/PlotVCFAAF_vs_Position.py
@@ -31,12 +31,14 @@ from pymodule import yh_matplotlib
 from pymodule import SNPData
 import numpy, csv, os, sys, re, gzip
 from pymodule.utils import runLocalCommand, getColName2IndexFromHeader
+from pymodule.VCFFile import VCFFile
 
 class PlotVCFAAF_vs_Position(object):
 	__doc__ = __doc__
 	option_default_dict = {('inputFname', 1, ): [None, 'i', 1, 'input VCF file'],\
 						('outputFname', 1, ): [None, 'o', 1, 'output file for the figure.'],\
 						('minNoOfTotal', 1, int): [100, '', 1, 'minimum no of total variants (denominator of inconsistent rate)'],\
+						('figureDPI', 1, int): [200, '', 1, 'dpi for the output figures (png)'],\
 						('title', 0, ): [None, 't', 1, 'title for the figure.'],\
 						('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
 						('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']}
@@ -50,8 +52,7 @@ class PlotVCFAAF_vs_Position(object):
 		self.ad = ProcessOptions.process_function_arguments(keywords, self.option_default_dict, error_doc=self.__doc__, \
 														class_to_have_attr=self)
 	
-	@classmethod
-	def getContig2Locus2Frequency(cls, inputFname, VCFOutputType=2):
+	def getLocusAndData(self, inputFname, VCFOutputType=2):
 		"""
 		2011-9-21
 		
@@ -68,69 +69,30 @@ class PlotVCFAAF_vs_Position(object):
 		else:
 			contig_id = os.path.splitext(os.path.split(inputFname)[1])[0]
 		
-		if inputFname[-3:]=='.gz':
-			import gzip
-			inf = gzip.open(inputFname)
-		else:
-			inf = open(inputFname)
-		reader = csv.reader(inf, delimiter='\t')
-		
+		vcfFile = VCFFile(inputFname=self.inputFname)
 		counter = 0
 		real_counter = 0
 		
 		locus_ls = []
-		position_ls = []
-		frequency_ls = []
+		xData_ls = []
+		yData_ls = []
 		
-		for row in reader:
-			if row[0] =='#CHROM':
-				row[0] = 'CHROM'	#discard the #
-				header = row
-				col_name2index = getColName2IndexFromHeader(header, skipEmptyColumn=True)
-				continue
-			elif row[0][0]=='#':	#2011-3-4
-				continue
-			"""
-			if chr_number_pattern.search(row[0]):
-				chr = chr_number_pattern.search(row[0]).group(1)
-			elif chr_pure_number_pattern.search(row[0]):
-				chr = chr_pure_number_pattern.search(row[0]).group(1)
-			else:
-				sys.stderr.write("Couldn't parse the chromosome number/character from %s.\n Exit.\n"%(row[0]))
-				sys.exit(4)
-			"""
-			chr = row[0]
-			pos = row[1]
-			quality = row[5]
+		for vcfRecord in vcfFile.parseIter():
+			locus = vcfRecord.locus
+			chr = vcfRecord.chr
+			pos = vcfRecord.pos
+			pos = int(pos)
 			
-			outputHet= False
+			AF1 = vcfRecord.info_tag2value.get("AF", vcfRecord.info_tag2value.get("AF1", None))
 			
-			info = row[7]
-			info_ls = info.split(';')
-			info_tag2value = {}
-			for info in info_ls:
-				try:
-					tag, value = info.split('=')
-				except:
-					#sys.stderr.write("Error in splitting %s by =.\n"%info)	###Error in splitting DS by =.
-					continue
-				info_tag2value[tag] = value
-			
-			current_locus = '%s_%s'%(chr, pos)
-			refBase = row[col_name2index['REF']]
-			altBase = row[col_name2index['ALT']]
-			if "AF1" in info_tag2value:
-				AF1 = info_tag2value.get("AF1")
-			else:
-				AF1 = info_tag2value.get("AF")
 			if AF1:
 				AF1 = float(AF1)
-				locus_ls.append(current_locus)
-				position_ls.append(int(pos))
-				frequency_ls.append(AF1)
+				locus_ls.append(locus)
+				xData_ls.append(pos)
+				yData_ls.append(AF1)
 		
-		sys.stderr.write("%s loci. Done.\n"%(len(frequency_ls)))
-		return PassingData(contig_id=contig_id, locus_ls=locus_ls, frequency_ls=frequency_ls, position_ls=position_ls)
+		sys.stderr.write("%s loci. Done.\n"%(len(yData_ls)))
+		return PassingData(contig_id=contig_id, locus_ls=locus_ls, yData_ls=yData_ls, xData_ls=xData_ls)
 	
 	@classmethod
 	def isLocusPolymorphicBasedOnAAF(cls, frequency, frequencyDelta=0.001):
@@ -154,14 +116,14 @@ class PlotVCFAAF_vs_Position(object):
 				these loci are not polymorphic.
 		"""
 		
-		passingData = self.getContig2Locus2Frequency(inputFname, VCFOutputType=2)
+		passingData = self.getLocusAndData(inputFname, VCFOutputType=2)
 		
 		if passingData is None:
 			return
 		
 		locus_ls = passingData.locus_ls
-		position_ls = passingData.position_ls
-		frequency_ls = passingData.frequency_ls
+		xData_ls = passingData.xData_ls
+		yData_ls = passingData.yData_ls
 		
 		import os, sys
 		from pymodule import yh_matplotlib
@@ -169,9 +131,9 @@ class PlotVCFAAF_vs_Position(object):
 		
 		import pylab
 		pylab.clf()
-		no_of_data_points = len(position_ls)
+		no_of_data_points = len(xData_ls)
 		if no_of_data_points>=min_no_of_data_points:
-			pylab.plot(position_ls, frequency_ls, ".",)
+			pylab.plot(xData_ls, yData_ls, ".",)
 			pylab.title('%s sites in Contig%s'%(no_of_data_points, passingData.contig_id))
 			if xlabel:
 				pylab.xlabel(xlabel)
@@ -187,7 +149,7 @@ class PlotVCFAAF_vs_Position(object):
 		
 		
 		self.plotVCFAlternativeAlleleFrequencyOverPosition(self.inputFname, self.outputFname, min_no_of_data_points=10, \
-													xlabel='contig position', ylabel='AAF', dpi=300, frequencyDelta=0.001)
+													xlabel='contig position', ylabel='AAF', dpi=self.figureDPI, frequencyDelta=0.001)
 		
 
 if __name__ == '__main__':
