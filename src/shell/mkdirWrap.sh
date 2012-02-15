9a3fd5e232ab2177ef40f431ba98a386bb5e68dd
crocea <crocea@uclaOffice>
2012-02-15 13:04:09 -0800

    stop redirecting stdout/stderr to /dev/null

--- src/shell/mkdirWrap.sh
+++ src/shell/mkdirWrap.sh
@@ -2,8 +2,9 @@
 # 201-8-26 wrap around mkdirhier/mkdir so that it won't report error when the directory already exists
 if test -x /usr/bin/mkdirhier
 then
-    /usr/bin/mkdirhier $1 >& /dev/null
+	echo "using mkdirhier"
+	/usr/bin/mkdirhier $1
 else
-    mkdir $1 >& /dev/null
+	mkdir -p $1
 fi
 echo "return code is $?"
