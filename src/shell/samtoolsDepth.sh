decd15b37885d9bcfebab323197435529b991f32
crocea <crocea@uclaOffice>
2012-09-24 10:07:06 -0700

    no more gzip of the samtools depth output. a bug in boost gzip library caused the downstream CalculateMedian... program to abort on some input after reading majority of input (>2 billion lines)

--- src/shell/samtoolsDepth.sh
+++ src/shell/samtoolsDepth.sh
@@ -7,12 +7,14 @@ then
 	echo "Usage: $0 samtoolsPath inputBam outputDepthFname [minMapQ] [minBaseQ] [moreSAMtoolsArguments]"
 	echo
 	echo "Note:"
-	echo "	1. run 'samtools depth -q minMapQ -Q minBaseQ' on the input bam and gzip the depth output."
+	echo "	1. run 'samtools depth -q minMapQ -Q minBaseQ' on the input bam."
+	echo "		The depth output is chr,pos,depth, tab-delimited. could be very large."
 	echo "	2. minMapQ is the minimum mapping quality for a read to be considered. default is $minMapQDefault."
 	echo "	3. minBaseQ is the minimum base quality for a base to be considered. default is $minBaseQDefault."
 	echo
 	echo "Example:"
 	echo "	$0 ~/bin/samtools input.bam output.depth.tsv.gz "
+	echo "	$0 ~/bin/samtools input.bam output.depth.tsv "
 exit
 fi
 
@@ -39,4 +41,5 @@ fi
 echo minMapQ: $minMapQ
 echo minBaseQ: $minBaseQ
 
-$samtoolsPath depth -q $minMapQ -Q $minBaseQ $arguments $inputBam | gzip > $outputDepthFname
+#$samtoolsPath depth -q $minMapQ -Q $minBaseQ $arguments $inputBam | gzip > $outputDepthFname
+$samtoolsPath depth -q $minMapQ -Q $minBaseQ $arguments $inputBam > $outputDepthFname
