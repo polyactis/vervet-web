122e2053c0c5ee8bb37b1eb780d4f9f7187c0aed
crocea <crocea@uclaOffice>
2012-12-02 18:46:44 -0800

    use site.latitude, site.longitude in all views

--- src/VervetDB.sql
+++ src/VervetDB.sql
@@ -1,9 +1,9 @@
 -- 2011-4-29
 
-drop view view_individual;
+drop view view_individual cascade;
 create or replace view view_individual as select i.id, i.code, i.name, i.ucla_id, i.tax_id, i.sex, i.age, i.age_cas, 
-    i.approx_age_group_at_collection, u.realname as collector, i.collection_date, i.latitude, 
-    i.longitude,i.site_id, i.target_coverage, s.short_name as site_name,
+    i.approx_age_group_at_collection, u.realname as collector, i.collection_date, s.latitude, 
+    s.longitude, i.site_id, i.target_coverage, s.short_name as site_name,
     s.city, s.stateprovince as province, s.country_id, c.name as country from individual i, site s, country c,
     acl_user u where i.site_id=s.id and s.country_id=c.id and i.collector_id=u.id;
     
@@ -15,7 +15,7 @@ create or replace view view_alignment as select i.id as individual_id, i.code, i
     ia.median_depth, ia.mean_depth, ia.mode_depth, ia.outdated_index, ia.individual_sequence_file_raw_id,
     ia.total_no_of_reads, 
     ia.date_created, ia.date_updated
-    from individual i, individual_alignment ia, individual_sequence isq where isq.individual_id=i.id and isq.id=ind_seq_id
+    from view_individual i, individual_alignment ia, individual_sequence isq where isq.individual_id=i.id and isq.id=ind_seq_id
     order by individual_id, isq.sequencer, isq.sequence_type, ref_ind_seq_id, alignment_id;
 
     
@@ -43,8 +43,8 @@ create or replace view view_individual_sequence as select ntt.*, t2.batch_id_lis
     isq.sequence_type, isq.format, i.target_coverage, isq.coverage, isq.path, isq.filtered, isq.quality_score_format, isq.tissue_id,
     isq.read_count, 
     i.id as individual_id, i.code, i.name, i.ucla_id, i.tax_id, i.sex, i.age, i.age_cas, 
-    i.approx_age_group_at_collection, i.collection_date, i.latitude, 
-    i.longitude, s.id as site_id, s.short_name as site_name, s.city, s.country_id, c.name as country, isq.date_created, isq.date_updated
+    i.approx_age_group_at_collection, i.collection_date, s.latitude, 
+    s.longitude, s.id as site_id, s.short_name as site_name, s.city, s.country_id, c.name as country, isq.date_created, isq.date_updated
     from individual i, individual_sequence isq, site s, country c 
     where i.site_id=s.id and i.id=isq.individual_id and s.country_id=c.id) 
     as newt left join tissue t on t.id=newt.tissue_id) as ntt, 
