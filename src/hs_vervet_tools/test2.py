5e420dad6db55b15c472d6ef05b8d6e314c5ed63
hannes.svardal <hannes.svardal@localhost>
2012-12-20 15:53:15 +0100

    only my functions updated

--- src/hs_vervet_tools/test2.py
+++ src/hs_vervet_tools/test2.py
@@ -1,12 +1,29 @@
-#import os
-#filename = os.path.join("/home/GMI", 'hannes.svardal/Documents')
+import wxversion
+#wxversion.select('2.8')
 
-#print filename
-import hsVCFExtractionTools
+import matplotlib
+matplotlib.use('Agg')
+import pylab
+import numpy as np
+#matplotlib.use('Qt4Agg')
 
-obj=hsVCFExtractionTools.hsVCFExtractionTools("TestAnalyses1")
-genotypeMethodID=40
 
-(VCFfilename_ls,contig_ls)=obj.loadVCFfilename_ls(genotypeMethodID=genotypeMethodID)
-print VCFfilename_ls
-print contig_ls
+n = 5
+X = np.random.normal(0,1,n)
+Y = np.random.normal(0,1,n)
+
+matplotlib.pyplot.scatter(X,Y)
+matplotlib.pyplot.show()
+#
+#import numpy
+#import pylab 
+#t = numpy.arange(0.0, 1.0+0.01, 0.01)
+#s = numpy.cos(2*2*numpy.pi*t)
+#pylab.plot(t, s) 
+#pylab.xlabel('time (s)')
+#pylab.ylabel('voltage (mV)')
+#pylab.title('About as simple as it gets, folks')
+#pylab.grid(True)
+#pylab.savefig('simple_plot')
+# 
+#pylab.show()
