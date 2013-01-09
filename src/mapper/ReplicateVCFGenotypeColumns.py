0af3ba70259323c773d07c45d5d0d2772a8310e9
crocea <crocea@uclaOffice>
2013-01-08 17:32:15 -0800

    fix some import errors

--- src/mapper/ReplicateVCFGenotypeColumns.py
+++ src/mapper/ReplicateVCFGenotypeColumns.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
 import csv
 from pymodule import ProcessOptions, getListOutOfStr, PassingData, utils, figureOutDelimiter, getColName2IndexFromHeader
-from pymodule.VCFFile import VCFFile
+from pymodule import VCFFile
 from AbstractVCFMapper import AbstractVCFMapper
 
 class ReplicateVCFGenotypeColumns(AbstractVCFMapper):
@@ -64,7 +64,6 @@ class ReplicateVCFGenotypeColumns(AbstractVCFMapper):
 			
 		"""
 		sys.stderr.write("Replicating some genotype columns in %s ...\n"%(inputFname))
-		from pymodule.VCFFile import VCFFile
 		vcfFile = VCFFile(inputFname=inputFname, minDepth=minDepth)
 		
 		outVCFFile = VCFFile(outputFname=outputFname)
