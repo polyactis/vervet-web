862cad13bd7a74a7e358a6c38d49958d16ced9a9
crocea <crocea@uclaOffice>
2013-06-13 14:44:42 -0700

    correct import path

--- src/mapper/ReplicateVCFGenotypeColumns.py
+++ src/mapper/ReplicateVCFGenotypeColumns.py
@@ -22,7 +22,7 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 import csv
 from pymodule import ProcessOptions, getListOutOfStr, PassingData, utils, figureOutDelimiter, getColName2IndexFromHeader
 from pymodule import VCFFile
-from AbstractVCFMapper import AbstractVCFMapper
+from pymodule import AbstractVCFMapper
 
 class ReplicateVCFGenotypeColumns(AbstractVCFMapper):
 	__doc__ = __doc__
