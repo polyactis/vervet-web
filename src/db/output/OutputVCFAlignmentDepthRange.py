446e4e3535b33ec0ef47e4dfd9d5088aed23fc00
crocea <crocea@uclaOffice>
2013-06-17 10:38:59 -0700

    finish the program

--- src/db/output/OutputVCFAlignmentDepthRange.py
+++ src/db/output/OutputVCFAlignmentDepthRange.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python
 """
 Examples:
-	#for plink
-	%s -i AlignmentToCall_AllVRC_vs_524_AllContig195Sites.2012.8.5T1549/samtools/Contig195.vcf.gz
-		-o /tmp/723VRC.tfam -u yh
+	%s -z localhost -u yh -o /tmp/alignmentDepth.tsv
 
-	#2013.1.3 for TrioCaller
-	%s -i /Network/Data/vervet/db/genotype_file/method_25/25999_VCF_24115_VCF_Contig520.filterByMaxSNPMissingRate.recode.vcf.gz
-		--outputFileFormat 2 -u yh -o /tmp/trioCaller_VRC.merlin --sampleID2FamilyCountFname=/tmp/sampleID2familyCount_y2.tsv
+	%s 
 	
-	#2013.1.3 output for polymutt
-	%s -i /Network/Data/vervet/db/genotype_file/method_25/25999_VCF_24115_VCF_Contig520.filterByMaxSNPMissingRate.recode.vcf.gz
-		--outputFileFormat 3 -u yh -o /tmp/polymutt_VRC.merlin --polymuttDatFname /tmp/datfile
-		--sampleID2FamilyCountFname=/tmp/sampleID2familyCount_y3.tsv --db_passwd secret
-	
+	%s 
 Description:
-	2013.04.08 this program takes a VCF file as input,
-		matches the sample IDs with IndividualAlignment entries inside db,
-		outputs minDepth & maxDepth & GQ of that sample, to be used by workflows such as FilterVCFPipeline.py
+	2013.06.13 this program outputs alignment depth, part of workflows such as FilterVCFPipeline.py
+		If inputFname (VCF file) is given, it takes a VCF file as input 
+			and matches the sample IDs with IndividualAlignment entries inside db.
+		Otherwise, it outputs all alignments (--ref_ind_seq_id).
 """
 
 import sys, os, math
@@ -26,10 +19,9 @@ __doc__ = __doc__%(sys.argv[0], sys.argv[0], sys.argv[0])
 sys.path.insert(0, os.path.expanduser('~/lib/python'))
 sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
-import csv
-import heapq, copy
-from pymodule import ProcessOptions, getListOutOfStr, PassingData, utils, figureOutDelimiter, NextGenSeq, Genome
-from pymodule import VCFFile
+import copy
+from pymodule import ProcessOptions
+from pymodule import MatrixFile
 from vervet.src.mapper.AbstractVervetMapper import AbstractVervetMapper
 from vervet.src import VervetDB
 
@@ -39,8 +31,10 @@ class OutputVCFAlignmentDepthRange(AbstractVervetMapper):
 	option_default_dict.pop(('outputFnamePrefix', 0, ))
 	option_default_dict.update({
 						('outputFname', 1, ):option_default_dict.get(('outputFname', 0, )),\
-						('depthFoldChange', 0, int): [2, '', 1, 'fold-change of median depth, determining minDepth & maxDepth in output'],\
-						("minGQ", 0, int): [30, '', 0, 'minimum genotype quality to output'],\
+						('depthFoldChange', 0, int): [2, '', 1, 'fold-change of median depth, determining minDepth & maxDepth for outputFileFormat 2'],\
+						("minGQ", 0, int): [30, '', 1, 'minimum genotype quality. for outputFileFormat 2'],\
+						('outputFileFormat', 0, int): [1, '', 1, 'tab-delimited. 1: read_group, median_depth; 2: read_group, minDepth, maxDepth, minGQ.'],\
+						("ref_ind_seq_id", 0, int): [None, '', 1, 'ID of the reference sequence used in alignment. used to filter alignments.'],\
 						})
 	option_default_dict.pop(('outputFname', 0, ))	#pop after its value has been used above
 	def __init__(self, inputFnameLs=None, **keywords):
@@ -48,32 +42,45 @@ class OutputVCFAlignmentDepthRange(AbstractVervetMapper):
 		"""
 		AbstractVervetMapper.__init__(self, inputFnameLs=inputFnameLs, **keywords)
 	
-	def outputAlignmentDepthAndOthersForFilter(self, db_vervet=None, outputFname=None, ref_ind_seq_id=524, \
-											foldChange=2, minGQ=30):
+	def outputAlignmentDepthAndOthersForFilter(self, db_vervet=None, inputFname=None, \
+						ref_ind_seq_id=None, depthFoldChange=2, minGQ=30, \
+						outputFname=None, outputFileFormat=1):
 		"""
 		2012.6.12
 			added argument db_vervet, moved from FilterVCFPipeline.py
 		2011-9-2
 		"""
-		sys.stderr.write("Outputting sequence coverage to %s ..."%outputFname)
-		import csv
+		sys.stderr.write("Outputting alignment (from %s) coverage to %s ..."%(inputFname, outputFname))
+		if inputFname:
+			alignmentLs = db_vervet.getAlignmentsFromVCFFile(inputFname=inputFname)
+		else:
+			TableClass = VervetDB.IndividualAlignment
+			query = TableClass.query.filter(TableClass.median_depth!=None)
+			if ref_ind_seq_id:
+				query = query.filter(TableClass.ref_ind_seq_id==ref_ind_seq_id)
+			alignmentLs = query.order_by(TableClass.id)
+			
+		writer = MatrixFile(inputFname=outputFname, openMode='w', delimiter='\t')
+		if outputFileFormat==1:
+			header = ['alignmentID', 'medianDepth']
+		else:
+			header = ['alignmentID', 'minDepth', 'maxDepth', 'minGQ']
+		writer.writeHeader(header)
+		
 		counter = 0
-		writer = csv.writer(open(outputFname, 'w'), delimiter='\t')
-		writer.writerow(['aln.id', 'minDepth', 'maxDepth', 'minGQ'])
-		TableClass = VervetDB.IndividualAlignment
-		query = TableClass.query.filter(TableClass.median_depth!=None)
-		if ref_ind_seq_id:
-			query = query.filter(TableClass.ref_ind_seq_id==ref_ind_seq_id)
-		query = query.order_by(TableClass.id)
-		for row in query:
-			minDepth = row.median_depth/float(foldChange)
-			if abs(minDepth-0)<=0.001:	#if it's too close to 0, regard it as 0.
-				minDepth = 0
-			read_group
-			writer.writerow([read_group, minDepth, \
-							row.median_depth*float(foldChange), minGQ])
+		for row in alignmentLs:
+			read_group = row.read_group
+			if outputFileFormat==1:
+				data_row = [read_group, row.median_depth]
+			else:
+				minDepth = row.median_depth/float(depthFoldChange)
+				if abs(minDepth-0)<=0.001:	#if it's too close to 0, assign 0.
+					minDepth = 0
+				data_row = [read_group, minDepth, \
+							row.median_depth*float(depthFoldChange), minGQ]
+			writer.writerow(data_row)
 			counter += 1
-		del writer
+		writer.close()
 		sys.stderr.write("%s entries fetched.\n"%(counter))
 	
 	def run(self):
@@ -84,9 +91,12 @@ class OutputVCFAlignmentDepthRange(AbstractVervetMapper):
 			import pdb
 			pdb.set_trace()
 		session = self.db_vervet.session
-		db_vervet = self.db_vervet
 		session.begin()
 		
+		self.outputAlignmentDepthAndOthersForFilter(db_vervet=self.db_vervet, \
+							inputFname=self.inputFname, ref_ind_seq_id=self.ref_ind_seq_id, \
+							depthFoldChange=self.depthFoldChange, minGQ=self.minGQ, \
+							outputFname=self.outputFname, outputFileFormat=self.outputFileFormat)
 	
 if __name__ == '__main__':
 	main_class = OutputVCFAlignmentDepthRange
